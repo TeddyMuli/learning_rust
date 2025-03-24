010223f97e0235bdf8930e39cd9181c2272a616a
TeddyMuli <tmuli974@gmail.com>
2025-03-24 20:08:29 +0300

    untrack builds

--- hello_cargo/target/debug/deps/hello_cargo-ccf190c558094328.d
+++
@@ -1,5 +0,0 @@
-/home/teddy/CODE/learning_rust/hello_cargo/target/debug/deps/hello_cargo-ccf190c558094328: src/main.rs
-
-/home/teddy/CODE/learning_rust/hello_cargo/target/debug/deps/hello_cargo-ccf190c558094328.d: src/main.rs
-
-src/main.rs:
--- hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/home/teddy/CODE/learning_rust/hello_cargo/target/debug/hello_cargo: /home/teddy/CODE/learning_rust/hello_cargo/src/main.rs
