cebd2def9e4653c9193ff1fbb029080ea07a6342
TeddyMuli <tmuli974@gmail.com>
2025-03-24 15:43:40 +0300

    create package with cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-0a0c5755cecea620.d
@@ -0,0 +1,5 @@
+/home/teddy/CODE/learning_rust/hello_cargo/target/debug/deps/libhello_cargo-0a0c5755cecea620.rmeta: src/main.rs
+
+/home/teddy/CODE/learning_rust/hello_cargo/target/debug/deps/hello_cargo-0a0c5755cecea620.d: src/main.rs
+
+src/main.rs:
